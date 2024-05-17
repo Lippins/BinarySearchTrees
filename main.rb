913f2803d1287623277d281c5a9c0e7842e3cf6b
Israel Ogunmola <israelogunmola@gmail.com>
2024-05-17 01:56:32 +0100

    feat: Update driver script

--- main.rb
+++ main.rb
@@ -2,78 +2,35 @@
 
 require_relative './tree'
 
-puts "Running driver script!\n"
-
-puts '#1. Creating a binary search tree from an array of random numbers between 1 and 100'
-sleep(2)
+# 1. Creating a binary search tree from an array of random numbers between 1 and 100
 elements = Array.new(15) { rand(1..100) }
 bst = Tree.new(elements)
-puts
-bst.pretty_print
 
-puts '#2. Confirming that the tree is balanced by calling `#balanced?`'
-sleep(2)
+# 2. Confirming that the tree is balanced by calling #balanced?
 puts bst.balanced? ? 'The tree is balanced!' : "The tree isn't balanced!"
-puts
 
-puts '#3. Printing out all elements in level, pre, post, and in order'
-puts
-puts 'Level Order Traversal'
-p bst.level_order
+# 3. Printing out all elements in level, pre, post, and in order
+puts 'Level order:', bst.level_order
+puts 'Preorder:', bst.preorder
+puts 'Inorder:', bst.inorder
+puts 'Postorder:', bst.postorder
 
-puts
-sleep(2)
-puts 'PreOrder Traversal'
-p bst.preorder
-
-puts
-sleep(2)
-puts 'InOrder Traversal'
-p bst.inorder
-
-puts
-sleep(2)
-puts 'PostOrder Traversal'
-p bst.postorder
-puts
-
-puts '#4. Unbalancing the tree by adding several numbers > 100'
-puts
+# 4. Unbalancing the tree by adding several numbers > 100
 (100..115).each { |num| bst.insert(num) }
+
+# 5. Confirming that the tree is balanced by calling #balanced?
+puts bst.balanced? ? 'The tree is balanced!' : "The tree isn't balanced!"
 bst.pretty_print
 
-puts '#5. Confirming that the tree is balanced by calling `#balanced?`'
-sleep(2)
-puts bst.balanced? ? 'The tree is balanced!' : "The tree isn't balanced!"
-puts
-
-puts '#6. Balancing the tree by calling `#rebalance`'
-puts
-sleep(2)
+# 6. Balancing the tree by calling #rebalance
 bst.rebalance
 bst.pretty_print
 
-puts '#7. Confirming that the tree is balanced by calling `#balanced?`'
-sleep(2)
+# 7. Confirming that the tree is balanced by calling #balanced?
 puts bst.balanced? ? 'The tree is balanced!' : "The tree isn't balanced!"
-puts
 
-puts '#8. Printing out all elements in level, pre, post, and in order'
-puts
-puts 'Level Order Traversal'
-p bst.level_order
-
-puts
-sleep(2)
-puts 'PreOrder Traversal'
-p bst.preorder
-
-puts
-sleep(2)
-puts 'InOrder Traversal'
-p bst.inorder
-
-puts
-sleep(2)
-puts 'PostOrder Traversal'
-p bst.postorder
+# 8. Printing out all elements in level, pre, post, and in order
+puts 'Level order:', bst.level_order
+puts 'Preorder:', bst.preorder
+puts 'Inorder:', bst.inorder
+puts 'Postorder:', bst.postorder
